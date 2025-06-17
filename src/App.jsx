696d63a11d191cf4f658494fe1455da7f41f7f88
Sahid Alam Khan <sahidalamkhan853@gmail.com>
2025-06-17 11:39:38 +0530

    Initial push to GitHub

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/navbar.jsx'  // Import with uppercase
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navbar />   
+      </div>
+    </>
+  )
+}
+
+export default App
